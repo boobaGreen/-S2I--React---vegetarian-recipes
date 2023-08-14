487684c15557054645ac59f7c1d517831e64430f
boobaGreen <devboobagreen@gmail.com>
2023-08-15 01:42:53 +0200

    netlify test env old test dai

--- src/feautures/recipes/useRecipe.js
+++ src/feautures/recipes/useRecipe.js
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { getRecipe } from "../../services/apiRecipe";
+import { getRecipe } from "../services/apiRecipe";
 
 export function useRecipe(id) {
   const {
