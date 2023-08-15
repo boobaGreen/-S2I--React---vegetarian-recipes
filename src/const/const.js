30fffb3b1dcf657180ef0e2b7f39b693eb1e14b4
boobaGreen <devboobagreen@gmail.com>
2023-08-15 17:41:30 +0200

    netlify prodkey

--- src/const/const.js
+++ src/const/const.js
@@ -7,7 +7,7 @@ export const baseURL = "https://api.spoonacular.com";
 // const ApiKey5 = "9df22f753f2b465b845ceec4b2a42cd5";
 // const ApiKey6 = "54c1854316a94c4393d9833bd25da51a";
 // const ApiKey7 = "ad017816015e44abbb55cfa553b95f92";
-// const ApiKey8 = "df4c9a66654a42ed97ea34824597d624";
-const ApiKey9 = "45714edb286e47f3a63be3536c8a87ee"; // usata in prod
-const ApiKey = ApiKey9;
+const ApiKey8 = "df4c9a66654a42ed97ea34824597d624";
+// const ApiKey9 = "45714edb286e47f3a63be3536c8a87ee"; // usata in prod
+const ApiKey = ApiKey8;
 export { ApiKey };
